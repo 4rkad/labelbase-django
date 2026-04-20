c1a7bbdb839c8f292499d2676d37fd52d8ddadae
4rkad <4rkad@users.noreply.github.com>
2026-04-20 16:44:31 +0200

    umbrel_update_profile: migrate stale 4rkad.2 defaults to new hostname scheme

--- django/umbrel_update_profile.py
+++ django/umbrel_update_profile.py
@@ -2,12 +2,29 @@ import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labellabor.settings")
 django.setup()
 from userprofile.models import Profile
+
 eh = os.environ.get("UMBREL_ELECTRUM_HOSTNAME")
 ep = os.environ.get("UMBREL_ELECTRUM_PORTS", "t50001")
 me = os.environ.get("UMBREL_MEMPOOL_ENDPOINT")
+
+# Any hostname the 4rkad Umbrel package has ever shipped as its Electrum default.
+ELECTRUM_STALE_HOSTS = (
+    "electrum.emzy.de",
+    "10.21.21.10",
+)
+MEMPOOL_STALE_ENDPOINTS = (
+    "https://mempool.space",
+    "http://10.21.21.26:3006",
+)
+
 if eh:
-    n = Profile.objects.filter(electrum_hostname="electrum.emzy.de").update(electrum_hostname=eh, electrum_ports=ep)
-    print(f"updated {n} profiles with electrum host {eh}")
+    n = Profile.objects.filter(
+        electrum_hostname__in=ELECTRUM_STALE_HOSTS
+    ).update(electrum_hostname=eh, electrum_ports=ep)
+    print(f"migrated {n} profiles to electrum host {eh}")
+
 if me:
-    n = Profile.objects.filter(mempool_endpoint="https://mempool.space").update(mempool_endpoint=me)
-    print(f"updated {n} profiles with mempool {me}")
+    n = Profile.objects.filter(
+        mempool_endpoint__in=MEMPOOL_STALE_ENDPOINTS
+    ).update(mempool_endpoint=me)
+    print(f"migrated {n} profiles to mempool {me}")
